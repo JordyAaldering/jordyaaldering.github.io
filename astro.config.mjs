35a6c00aeca9907dc3abe2e7fe8e8f323391ab27
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-03-11 20:32:13 +0100

    move first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// https://docs.astro.build/en/guides/integrations-guide/mdx/
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jordyaaldering.github.io/',
+  integrations: [mdx()],
 });
