ea27afc88808f8ac3402878787ff3fc9e476e400
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-03-16 14:10:03 +0100

    remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://jordyaaldering.github.io/',
-  integrations: [
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx()],
 	i18n: {
     locales: ['en', 'nl'],
     defaultLocale: 'en',
