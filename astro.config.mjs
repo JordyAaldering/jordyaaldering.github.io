50669e8a26c9ebab560c023b10559d97ad77392b
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-03-16 15:31:16 +0100

    pretty code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://jordyaaldering.github.io/',
   integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },
 	i18n: {
     locales: ['en', 'nl'],
     defaultLocale: 'en',
