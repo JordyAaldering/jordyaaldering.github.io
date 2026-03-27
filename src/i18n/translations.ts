944746823670940124365854b971494d1a93ecc4
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-03-27 16:10:57 +0100

    the svg thing was overkill

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -4,15 +4,11 @@ export const translations: { [key: string]: { [key: string]: string } } = {
     'nav.research': 'Research',
     'nav.teaching': 'Teaching',
     'nav.blog': 'Blog',
-    'plot.runtime': 'Time',
-    'plot.energy': 'Energy',
   },
   nl: {
     'nav.home': 'Home',
     'nav.research': 'Onderzoek',
     'nav.teaching': 'Onderwijs',
     'nav.blog': 'Blog',
-    'plot.runtime': 'Tijd',
-    'plot.energy': 'Energie',
   },
 } as const;
